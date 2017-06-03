b016a29f166f24454b82348e4549b604068fdbb9
master <nsvetlov@outlook.com>
2017-06-03 14:16:27 +0700

    Перепроведение платежа из блок-листа на спецкарту Signed-off-by: master <nsvetlov@outlook.com>

---
+++ Oldi.Net/GWRequestBlocklistRedo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oldi.Net
+{
+    public partial class GWRequest : IDisposable
+    {
+
+        /// <summary>
+        /// Обеспечивает перепроведение платежа из Bloklist на спецкарту
+        /// </summary>
+        void BlockListRedo()
+        {
+
+        }
+
+    }
+}
